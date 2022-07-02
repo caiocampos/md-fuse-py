852dbf371ddb322c73e8daed2625fff74d58c3da
Caio Campos <20524962+caiocampos@users.noreply.github.com>
2022-07-02 14:30:44 -0300

    Melhorada padronização

--- fuse/process.py
+++ fuse/process.py
@@ -2,24 +2,24 @@
 Módulo para processar os dados e gerar o arquivo final
 """
 
-from . import configuration
-from . import file
+from .configuration import load as config_load
+from .file import generate_text, write_text
 
 
 def process(conf_file_path: str):
     """
     Função para processar os dados e gerar o arquivo final
     """
-    conf = configuration.load(conf_file_path)
+    conf = config_load(conf_file_path)
 
     for file_conf in conf.files:
         print(f"Processando {file_conf.name}")
-        text = file.generate_text(conf.input_folder, file_conf.inputs)
+        text = generate_text(conf.input_folder, file_conf.inputs)
         if text:
             print("Arquivos lidos com sucesso!")
         else:
             continue
 
-        res = file.write_text(conf.output_folder, file_conf.output, text)
+        res = write_text(conf.output_folder, file_conf.output, text)
         if res:
             print(f"Arquivo {res} gravado com sucesso!")
